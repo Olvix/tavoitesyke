7d2991d85977622a95414c5bf2b127e1b66fb7c6
AAPPO_LAUNONEN <aappo.launonen@outlook.com>
2022-09-30 10:32:16 +0300

    Updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [age, setAge] = useState('')
+  const [heartrateUp, setHeartrateUp] = useState('')
+  const [heartrateLow, setHeartraeLow] = useState('')
+
+  function laske(e) {
+    e.preventDefault()
+    const upper = (200 - age) * 0.85
+    const lower = (200 - age) * 0.65
+    setHeartrateUp(upper)
+    setHeartraeLow(lower)
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div id="content">
+        <h3>Heart rate limits calculator</h3>
+        <form onSubmit={laske}>
+          <div>
+            <label>Age</label>
+            <input value={age} onChange={e => setAge(e.target.value)}/>
+          </div>
+          <div>
+            <label>Heart rate limits</label>
+            <output>{heartrateUp} - {heartrateLow}</output>
+          </div>
+          <button>Calculate</button>
+        </form>
+      </div>
   );
 }
 
